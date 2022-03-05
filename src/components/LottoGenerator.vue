d030ff8161d38b847777497994fbfb943624af3c
jay calamba <jaycalambastudio@gmail.com>
2022-03-05 13:48:51 +0700

    first commit

---
+++ src/components/LottoGenerator.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+      <img src="../assets/lotto.png" alt="lotto">
+      <h1>My Lotto Generator</h1>
+  </div>
+  <button @click="generate()">Generate Numbers</button>
+  <div class="container">
+      <div class="output" v-for="digit in randomDigits" :key="digit">
+          {{ digit }}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            randomDigits : [],
+            isPlaying : false
+        }
+    },
+    methods: {
+        generate(){
+
+            while(this.randomDigits.length < 6){
+                let digits = Math.floor((Math.random() * 41)) + 1;
+                if(!this.randomDigits.includes(digits)){
+                    this.randomDigits.push(digits);
+                }
+            }
+        }
+    }
+
+}
+</script>
+
+<style scope>
+.container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+}
+img {
+ height: 60px;
+ padding: 5px;
+}
+
+button{
+    margin-bottom: 10px;
+}
+
+.output {
+    padding: 5px;
+    font-weight: bold;
+}
+</style>
